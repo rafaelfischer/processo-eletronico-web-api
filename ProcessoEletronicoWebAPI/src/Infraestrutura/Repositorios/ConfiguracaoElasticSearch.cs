aa4f74690ba100fbae16d0915239dfde0e040ef2
Breno Wruck Schneider <breno.schneider@prodest.es.gov.br>
2016-08-19 15:31:03 -0300

    Adicionado o Elastic Search (com os métodos de abertura de conexão) utilizando o servidor em que está o índice do processoeletrônico.

---
+++ ProcessoEletronicoWebAPI/src/Infraestrutura/Repositorios/ConfiguracaoElasticSearch.cs
@@ -0,0 +1,30 @@
+﻿using Nest;
+using Elasticsearch.Net;
+using System;
+
+namespace ProcessoEletronicoService.Infraestrutura.Repositorios
+{
+    public static class ConfiguracaoElasticSearch
+    {
+        public static string urlElastic = "http://es-dev.labs.prodest.dcpr.es.gov.br";
+                
+        public static ElasticClient abrirConexao (string url, string indicePadrao = "processoeletronico")
+        {
+            Uri node = new Uri(url);
+            ConnectionSettings configuracoes = new ConnectionSettings(node).DefaultIndex(indicePadrao);
+            ElasticClient conexaoElastic = new ElasticClient(configuracoes);
+
+            return conexaoElastic;
+        }
+
+        public static ElasticLowLevelClient abrirConexaoLowLevel (string url, string indicePadrao = "/processoeletronico")
+        {
+            Uri node = new Uri(url + indicePadrao);
+            ConnectionConfiguration configuracoes = new ConnectionConfiguration(node);
+            ElasticLowLevelClient conexaoElastic = new ElasticLowLevelClient(configuracoes);
+            
+            return conexaoElastic;
+        }
+
+    }
+}
--- ProcessoEletronicoWebAPI/src/Negocio/Publico/ConsultaProcesso.cs
+++ ProcessoEletronicoWebAPI/src/Negocio/Publico/ConsultaProcesso.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ProcessoEletronicoService.Negocio.Publico.Base;
+using ProcessoEletronicoService.Infraestrutura.Repositorios.Base;
+
 
 namespace ProcessoEletronicoService.Negocio.Publico
 {
     public class ConsultaProcesso : IConsultaProcesso
     {
+
+        private IConsultaProcessoRepositorio repositorioProcesso;
+
+        public ConsultaProcesso(IConsultaProcessoRepositorio repositorioProcesso)
+        {
+            this.repositorioProcesso = repositorioProcesso;
+        }
+            
         public string ConsultarPorNumero(string numeroProcesso)
         {
-            return "Negócio - Consulta de Processo por Número. Processo número " + numeroProcesso;
+            return repositorioProcesso.ConsultarProcessoPorNumero(numeroProcesso);
         }
     }
 }
