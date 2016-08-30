ef424872a472e839c1f5ccc03bc5028f2218af97
vizeke <vizeke@gmail.com>
2016-08-30 16:37:33 -0300

    Corrige o travis para um nome de imagem do docker com menos de 30 caracteres

--- .develop.sh
+++ .develop.sh
@@ -4,6 +4,7 @@ docker rename $DOCKER_IMAGE "$DOCKER_IMAGE-dev"
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push "$DOCKER_IMAGE-dev"
+
 git clone https://github.com/prodest/gerencio-upgrade.git
 cd gerencio-upgrade
 npm install
