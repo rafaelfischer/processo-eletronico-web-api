4b293babdcd29f38a6be803a7b4f79149b528358
vizeke <vizeke@gmail.com>
2016-08-30 16:21:09 -0300

    Primeira tentativa de correçao do travis

--- .develop.sh
+++ .develop.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
  
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker rename $DOCKER_IMAGE "$DOCKER_IMAGE-dev"
+
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push "$DOCKER_IMAGE-dev"
 git clone https://github.com/prodest/gerencio-upgrade.git
 cd gerencio-upgrade
