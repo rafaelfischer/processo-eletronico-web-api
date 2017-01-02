bcf1870724da57e62fc919dae7a7012806e7802d
Breno Wruck Schneider <breno.schneider@prodest.es.gov.br>
2017-01-02 17:03:04 -0200

    Health Check da Api.

---
+++ ProcessoEletronicoWebAPI/src/WebAPI/Controllers/PingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProcessoEletronicoService.Apresentacao.Base;
+using ProcessoEletronicoService.Apresentacao.Modelos;
+using ProcessoEletronicoService.Infraestrutura.Comum;
+using ProcessoEletronicoService.WebAPI.Base;
+using ProcessoEletronicoService.WebAPI.Config;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace ProcessoEletronicoService.WebAPI.Controllers
+{
+    [Route("api/ping")]
+    public class PingController
+    {
+        /// <summary>
+        /// Health check.
+        /// </summary>
+        /// <returns>Pong.</returns>
+        /// <response code="200">Pong.</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(string), 200)]
+        public IActionResult Get()
+        {
+            return new ObjectResult("pong");
+        }
+    }
+}
