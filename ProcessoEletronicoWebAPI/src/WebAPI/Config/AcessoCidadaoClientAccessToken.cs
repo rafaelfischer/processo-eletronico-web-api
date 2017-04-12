91b63460460cf1cbb5ba398f28058c6b4da405ec
Fernando Fontoura de Almeida <ffalmeida@gmail.com>
2017-04-12 15:16:41 -0300

    Correção na forma de como obter o accesstoken e armazená-lo.

--- ProcessoEletronicoWebAPI/src/WebAPI/Config/AcessoCidadaoClientAccessToken.cs
+++ ProcessoEletronicoWebAPI/src/WebAPI/Config/AcessoCidadaoClientAccessToken.cs
@@ -2,18 +2,23 @@
 using Microsoft.Extensions.Options;
 using ProcessoEletronicoService.WebAPI.Base;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ProcessoEletronicoService.WebAPI.Config
 {
     public class AcessoCidadaoClientAccessToken : IClientAccessToken
     {
+        private readonly string _clientId;
+        private readonly string _secret;
         private readonly IOptions<AutenticacaoIdentityServer> _autenticacaoIdentityServerConfig;
+        private string _accessToke;
+        private DateTime? _halfExpireTime;
+        private DateTime? _expireTime;
 
         public AcessoCidadaoClientAccessToken(IOptions<AutenticacaoIdentityServer> autenticacaoIdentityServerConfig)
         {
+            _clientId = Environment.GetEnvironmentVariable("ProcessoEletronicoApiClientId");
+            _secret = Environment.GetEnvironmentVariable("ProcessoEletronicoApiSecret");
             _autenticacaoIdentityServerConfig = autenticacaoIdentityServerConfig;
         }
 
@@ -21,12 +26,7 @@ namespace ProcessoEletronicoService.WebAPI.Config
         {
             get
             {
-                TokenResponse tokenResponse = GetOrganogramaAccessTokenAsync().Result;
-
-                if (tokenResponse.IsError)
-                    throw new Exception(tokenResponse.Error, tokenResponse.Exception);
-
-                return tokenResponse.AccessToken;
+                return GetAccessTokenAsync().Result;
             }
         }
 
@@ -34,9 +34,50 @@ namespace ProcessoEletronicoService.WebAPI.Config
         {
             AutenticacaoIdentityServer autenticacaoIdentityServer = _autenticacaoIdentityServerConfig.Value;
 
-            TokenClient tokenClient = new TokenClient(autenticacaoIdentityServer.Authority + "/connect/token", "805ac3fd-3913-4742-9f27-1fa41e7cce9d", "xg4kEtyg%I%xe5OqfsROJvcGD9tcn**C");
+            TokenClient tokenClient = new TokenClient(autenticacaoIdentityServer.Authority + "/connect/token", _clientId, _secret);
 
             return await tokenClient.RequestClientCredentialsAsync("ApiOrganograma");
+        }
+
+        private async Task<string> GetAccessTokenAsync()
+        {
+            if (string.IsNullOrWhiteSpace(_accessToke))
+            {
+                //Caso o access token ainda não possua valor ele será obitido de forma síncrona.
+                await SetAccessTokenAsync();
+            }
+
+            DateTime now = DateTime.Now;
+
+            if (_halfExpireTime.HasValue && _halfExpireTime.Value <= now && _expireTime.HasValue && _expireTime.Value > now)
+            {
+                //Caso o access token já tenha passado da metade do tempo de expiração, mas ainda não tenha chegado no limite do tempo de expiração
+                //o access token será renovado de forma assincrona de forma que o usuário não fique esperando esta operação.
+                SetAccessTokenAsync();
+            }
+            else if (_expireTime.HasValue && _expireTime.Value <= now)
+            {
+                //Caso o access token já tenha atingido do tempo limite de expiração
+                //o access token será renovado de forma síncrona.
+                await SetAccessTokenAsync();
+            }
+
+            return _accessToke;
+        }
+
+        private async Task SetAccessTokenAsync()
+        {
+            TokenResponse tokenResponse = await GetOrganogramaAccessTokenAsync();
+
+            if (tokenResponse.IsError)
+                throw new Exception(tokenResponse.Error, tokenResponse.Exception);
+
+            //Tempo de expiração subtraído de 5 minutos para tolerância de relógios dessincronizados
+            _expireTime = DateTime.Now.AddSeconds((tokenResponse.ExpiresIn - (5 * 60)));
+            //Metade do tempo de expiração
+            _halfExpireTime = DateTime.Now.AddSeconds((tokenResponse.ExpiresIn / 2));
+            //AccessToken
+            _accessToke = tokenResponse.AccessToken;
         }
     }
 }
