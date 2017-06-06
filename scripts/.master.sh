7e8b6189570fb1f6d6d43f4405ef2be2ae3237be
alvarollmenezes <alvarollmenezes@gmail.com>
2017-06-06 12:10:16 -0300

    Teste de modularização do script do travis

--- scripts/.master.sh
+++ scripts/.master.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-docker tag $DOCKER_IMAGE $DOCKER_IMAGE:$DOCKER_TAG
+export RANCHER_STACK=prd
+export IMAGE_NAME=$DOCKER_IMAGE:$DOCKER_TAG
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push $DOCKER_IMAGE:$DOCKER_TAG
-
-#Atualiza a infra
-git clone https://github.com/prodest/api-cloud-v2.git
-cd api-cloud-v2
-npm install
-node ./client --ENVIRONMENT=SEP/Organograma \
-    --STACK=prd --SERVICE=processoeletronico-api \
-    --IMAGE=$DOCKER_IMAGE:$DOCKER_TAG --START_FIRST=true
+. ./.deploy.sh
--- scripts/.travis.sh
+++ scripts/.travis.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+export RANCHER_ENV=SEP/Organograma
+export RANCHER_SERVICE=processoeletronico-api
+export RANCHER_START_FIRST=true
+export DOCKER_IMAGE=prodest/processoeletronico-api
+export DOCKER_TAG=${TRAVIS_COMMIT:0:7}
+
 cd ProcessoEletronicoWebAPI/src/WebAPI/
 dotnet restore && dotnet publish -c release -r debian.8-x64 -o publish ./
 
 cd ../../../
 
 docker build -t $DOCKER_IMAGE -f ./Dockerfile .
-export DOCKER_TAG=${TRAVIS_COMMIT:0:7}
