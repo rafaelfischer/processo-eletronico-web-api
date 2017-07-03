c81e1beff45eb94b7dcf37a1029a19655b430051
Breno Wruck Schneider <breno.schneider@prodest.es.gov.br>
2017-07-03 09:40:30 -0300

    Atualizado script de deploy de homologacao

--- scripts/.release.sh
+++ scripts/.release.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-docker tag $DOCKER_IMAGE $DOCKER_IMAGE-hmg
+export RANCHER_STACK=dev
+export IMAGE_NAME=$DOCKER_IMAGE-hmg:$DOCKER_TAG
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push $DOCKER_IMAGE-hmg
-
-export RANCHER_ENV_ID=1a10541 #env processoeletronico (1a10541)
-export RANCHER_STACK_ID=1e102 #stack hmg (1e102)
-export RANCHER_STACK=hmg #stack hmg (1e102)
-
-export RANCHER_URL=http://cloud.datacenter.es.gov.br.local/v1/projects/$RANCHER_ENV_ID
-export RANCHER_COMPOSE_URL=http://cloud.datacenter.es.gov.br.local/v1/projects/$RANCHER_ENV_ID/environments/$RANCHER_STACK_ID/composeconfig
-
-#Atualiza a infra
-git clone https://github.com/prodest/gerencio-upgrade.git
-cd gerencio-upgrade
-npm install
-node ./client $RANCHER_SERVICE_NAME 40000
+. ./scripts/.deploy.sh
