18c52cb871d9de73640dd37f55e35effb5db4373
Breno Wruck Schneider <breno.schneider@prodest.es.gov.br>
2017-07-04 16:59:29 -0300

    Documetação de anexos de rascunhos de processos concluída

--- ProcessoEletronicoWebAPI/src/WebAPI/Rascunhos/Processo/AnexosController.cs
+++ ProcessoEletronicoWebAPI/src/WebAPI/Rascunhos/Processo/AnexosController.cs
@@ -1,9 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
-using ProcessoEletronicoService.Apresentacao.Modelos;
 using ProcessoEletronicoService.Negocio.Modelos;
 using ProcessoEletronicoService.Negocio.Rascunho.Processo.Base;
 using ProcessoEletronicoService.WebAPI.Base;
@@ -24,24 +22,67 @@ namespace ProcessoEletronicoService.WebAPI.Rascunhos.Processo
             _mapper = mapper;
             _negocio = negocio;
         }
-
-
+        /// <summary>
+        /// Lista de anexos do rascunho de processos
+        /// </summary>
+        /// <remarks>
+        /// O contéudo dos anexos não é enviado. O conteúdo é disponibilizado apenas na consulta por identificador.
+        /// </remarks>
+        /// <param name="idRascunhoProcesso">Identificador do Rascunho de processos</param>
+        /// <returns>Anexos do rascunho de processos</returns>
+        /// <response code="200">Retorna a lista de anexos</response>
+        /// <response code="404">Recurso não encontrado</response>
+        /// <response code="500">Falha inesperada</response>
         [HttpGet]
         [ApiExplorerSettings(GroupName = Constants.RascunhosDocumentationGroup)]
+        [ProducesResponseType(typeof(List<GetAnexoDto>), 200)]
+        [ProducesResponseType(typeof(string), 404)]
+        [ProducesResponseType(typeof(string), 500)]
         public IActionResult Get(int idRascunhoProcesso)
         {
             return Ok(_mapper.Map<List<GetAnexoDto>>(_negocio.Get(idRascunhoProcesso)));
         }
 
+        /// <summary>
+        /// Anexo do rascunho de processos
+        /// </summary>
+        /// <remarks>
+        /// Essa consulta retorna o conteúdo do anexo
+        /// </remarks>
+        /// <param name="idRascunhoProcesso">Identificador do Rascunho de processos</param>
+        /// <param name="id">Identificador do Anexo</param>
+        /// <returns>Anexo de acordo com o identificador informado</returns>
+        /// <response code="200">Anexo de acordo com o identificador informado</response>
+        /// <response code="404">Recurso não encontrado</response>
+        /// <response code="500">Falha inesperada</response>
         [HttpGet("{id}", Name = "GetAnexo")]
+        [ProducesResponseType(typeof(List<GetAnexoDto>), 200)]
+        [ProducesResponseType(typeof(string), 404)]
+        [ProducesResponseType(typeof(string), 500)]
         [ApiExplorerSettings(GroupName = Constants.RascunhosDocumentationGroup)]
         public IActionResult Get(int idRascunhoProcesso, int id)
         {
             return Ok(_mapper.Map<GetAnexoDto>(_negocio.Get(idRascunhoProcesso, id)));
         }
-        
+
+        /// <summary>
+        /// Inserção de anexos no rascunho de processos
+        /// </summary>
+        /// <param name="idRascunhoProcesso">Identificador do Rascunho de processos</param>
+        /// <param name="postAnexoDto">Informações do anexo a ser inserido</param>
+        /// <returns>Anexo recém inserido</returns>
+        /// <response code="201">Anexo recém inserido</response>
+        /// <response code="400">Bad Request</response>
+        /// <response code="404">Recurso não encontrado</response>
+        /// <response code="422">Objeto não processável</response>
+        /// <response code="500">Falha inesperada</response>
         [HttpPost]
         [Authorize(Policy = "RascunhoProcesso.Rascunhar")]
+        [ProducesResponseType(typeof(GetAnexoDto), 201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(typeof(string), 404)]
+        [ProducesResponseType(typeof(string), 422)]
+        [ProducesResponseType(typeof(string), 500)]
         [ApiExplorerSettings(GroupName = Constants.RascunhosDocumentationGroup)]
         public IActionResult Post (int idRascunhoProcesso, [FromBody] PostAnexoDto postAnexoDto)
         {
@@ -56,8 +97,23 @@ namespace ProcessoEletronicoService.WebAPI.Rascunhos.Processo
             return CreatedAtRoute("GetAnexo", new { Id = getAnexoDto.Id }, getAnexoDto);
         }
 
+        /// <summary>
+        /// Alteração de um anexo do rascunho de processos
+        /// </summary>
+        /// <param name="idRascunhoProcesso">Identificador do Rascunho de processos</param>
+        /// <param name="patchAnexo">Informações a serem alteradas (JSON Patch Document)</param>
+        /// <response code="204">Alteração feita com sucesso</response>
+        /// <response code="400">Bad Request</response>
+        /// <response code="404">Recurso não encontrado</response>
+        /// <response code="422">Objeto não processável</response>
+        /// <response code="500">Falha inesperada</response>
         [HttpPatch("{id}")]
         [Authorize(Policy = "RascunhoProcesso.Rascunhar")]
+        [ProducesResponseType(typeof(void), 204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(typeof(string), 404)]
+        [ProducesResponseType(typeof(string), 422)]
+        [ProducesResponseType(typeof(string), 500)]
         [ApiExplorerSettings(GroupName = Constants.RascunhosDocumentationGroup)]
         public IActionResult Patch (int idRascunhoProcesso, int id, [FromBody] JsonPatchDocument<PatchAnexoDto> patchAnexo)
         {
@@ -77,8 +133,19 @@ namespace ProcessoEletronicoService.WebAPI.Rascunhos.Processo
             return NoContent();
         }
 
+        /// <summary>
+        /// Exclusão de um anexo do rascunho de processos
+        /// </summary>
+        /// <param name="idRascunhoProcesso">Identificador do Rascunho de processos</param>
+        /// <param name="id">Identificador do anexo a ser excluído</param>
+        /// <response code="204">Exclusão feita com sucesso</response>
+        /// <response code="404">Recurso não encontrado</response>
+        /// <response code="500">Falha inesperada</response>
         [HttpDelete("{id}")]
         [Authorize(Policy = "RascunhoProcesso.Rascunhar")]
+        [ProducesResponseType(typeof(void), 204)]
+        [ProducesResponseType(typeof(string), 404)]
+        [ProducesResponseType(typeof(string), 500)]
         [ApiExplorerSettings(GroupName = Constants.RascunhosDocumentationGroup)]
         public IActionResult Delete(int idRascunhoProcesso, int id)
         {
