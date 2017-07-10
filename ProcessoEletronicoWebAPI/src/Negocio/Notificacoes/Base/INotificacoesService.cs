175c750257dde7b40c0a041c6472bb8d80cfc3dc
Breno Wruck Schneider <breno.schneider@prodest.es.gov.br>
2017-07-10 15:52:39 -0300

    Criado Service de Notificacao

---
+++ ProcessoEletronicoWebAPI/src/Negocio/Notificacoes/Base/INotificacoesService.cs
@@ -0,0 +1,14 @@
+﻿using ProcessoEletronicoService.Dominio.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Negocio.Notificacoes.Base
+{
+    public interface INotificacoesService
+    {
+        IList<Notificacao> Get();
+        void Post(int idProcesso, int idDespacho, string email);
+        void Run();
+    }
+}
---
+++ ProcessoEletronicoWebAPI/src/Negocio/Notificacoes/NotificacoesService.cs
@@ -0,0 +1,62 @@
+﻿using Negocio.Notificacoes.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ProcessoEletronicoService.Dominio.Modelos;
+using ProcessoEletronicoService.Dominio.Base;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Negocio.Notificacoes
+{
+    public class NotificacoesService : INotificacoesService
+    {
+        private string emailPadraoProcesso = "Notificacao de Processo";
+        private string emailPadraoDespacho = "Notificacao de Despacho";
+
+        private IRepositorioGenerico<Notificacao> _repositorioNotificacoes;
+        private IUnitOfWork _unityOfWork;
+
+        public NotificacoesService(IProcessoEletronicoRepositorios repositorios)
+        {
+            _repositorioNotificacoes = repositorios.Notificacoes;
+            _unityOfWork = repositorios.UnitOfWork;
+        }
+
+        public IList<Notificacao> Get()
+        {
+            return _repositorioNotificacoes.Where(n => n.DataNotificacao == null).ToList();
+        }
+
+        public void Post(int idProcesso, int idDespacho, string email)
+        {
+            Notificacao notificacao = new Notificacao { IdProcesso = idProcesso, IdDespacho = idDespacho, Email = email };
+            _repositorioNotificacoes.Add(notificacao);
+        }
+
+        public void Run()
+        {
+            IList<Notificacao> notificacoes = Get();
+            foreach (Notificacao notificacao in notificacoes)
+            {
+                if (!notificacao.IdDespacho.HasValue)
+                {
+                    NotificarAutuacao(notificacao);
+                } else
+                {
+                    NotificarDespacho(notificacao);
+                }
+}
+        }
+
+        private void NotificarAutuacao(Notificacao notificacao)
+        {
+            
+        }
+
+        private void NotificarDespacho(Notificacao notificacao)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
