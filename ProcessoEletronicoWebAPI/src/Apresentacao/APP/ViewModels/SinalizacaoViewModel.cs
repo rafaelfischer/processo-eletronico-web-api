a976fde6c6f69d1806b711fff0f0ae31519c95de
Breno Wruck Schneider <brenows@gmail.com>
2017-11-17 14:33:16 -0200

    Criada tela de lista de sinalizações. Closes #242

--- ProcessoEletronicoWebAPI/src/Apresentacao/APP/ViewModels/SinalizacaoViewModel.cs
+++ ProcessoEletronicoWebAPI/src/Apresentacao/APP/ViewModels/SinalizacaoViewModel.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Apresentacao.APP.ViewModels
+﻿namespace Apresentacao.APP.ViewModels
 {
     public class SinalizacaoViewModel
     {
         public int Id { get; set; }
         public string Descricao { get; set; }        
-    }    
+    }
+        
 }
