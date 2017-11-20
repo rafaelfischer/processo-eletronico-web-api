8748dd985e2f9d4a4fe167ec85a856641ef0dfba
Breno Wruck Schneider <brenows@gmail.com>
2017-11-20 10:01:22 -0200

    Alteração no script do Travis para publicar APP

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -7,8 +7,8 @@ dotnet publish -c release -r debian.8-x64 -o publish ./
 
 cd ../../../
 
-#cd ProcessoEletronicoWebAPI/src/WebAPP/
-#dotnet restore 
-#dotnet publish -c release -r debian.8-x64 -o publish ./
+cd ProcessoEletronicoWebAPI/src/WebAPP/
+dotnet restore 
+dotnet publish -c release -r debian.8-x64 -o publish ./
 
-#cd ../../../
+cd ../../../
