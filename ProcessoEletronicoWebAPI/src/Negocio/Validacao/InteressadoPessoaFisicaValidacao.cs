8d052db39d6a0cb4cd46c51c4e2ea978873e60f2
Breno Wruck Schneider <brenows@gmail.com>
2017-11-21 09:37:06 -0200

    Melhoria nas mensagens de validação de interessado PF e PJ na autução de processo

--- ProcessoEletronicoWebAPI/src/Negocio/Validacao/InteressadoPessoaFisicaValidacao.cs
+++ ProcessoEletronicoWebAPI/src/Negocio/Validacao/InteressadoPessoaFisicaValidacao.cs
@@ -81,7 +81,7 @@ namespace ProcessoEletronicoService.Negocio.Validacao
         {
             if (string.IsNullOrWhiteSpace(interessado.GuidMunicipio))
             {
-                throw new RequisicaoInvalidaException("Identificador do município do interessado não preenchido.");
+                throw new RequisicaoInvalidaException($"Identificador do município do interessado {interessado.Nome} não preenchido.");
             }
         }
 
@@ -119,7 +119,7 @@ namespace ProcessoEletronicoService.Negocio.Validacao
             }
             catch (Exception)
             {
-                throw new RequisicaoInvalidaException("Identificador do município do interessado inválido.");
+                throw new RequisicaoInvalidaException($"Identificador do município {interessado.GuidMunicipio} do interessado {interessado.Nome} inválido.");
             }
         }
 
