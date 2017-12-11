c3fd9eeaed66afa7e32b2a3404264f97affca6a6
Breno Wruck Schneider <brenows@gmail.com>
2017-12-11 14:03:44 -0200

    Alterado callback path

--- ProcessoEletronicoWebAPI/src/WebAPP/Startup.cs
+++ ProcessoEletronicoWebAPI/src/WebAPP/Startup.cs
@@ -119,7 +119,8 @@ namespace WebAPP
 
                 // Set the callback path, so Auth0 will call back to http://localhost:5000/signin-auth0 
                 // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard 
-                CallbackPath = new PathString("/signin-oidc"),
+                //CallbackPath = new PathString("/signin-oidc"),
+                CallbackPath = "http://processoeletronico.des.es.gov.br/signin-oidc",
 
                 // Configure the Claims Issuer to be Auth0
                 ClaimsIssuer = "processoeletronico",
